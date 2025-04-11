7beb490975f3a5262012c5dacb09d3e5fadc766c
Yuki Kurata <k024g0109@g.neec.ac.jp>
2025-04-12 08:04:42 +0900

    レンダリングパイプラインVer2

--- Matrix4x4.h
+++ Matrix4x4.h
@@ -24,6 +24,19 @@ struct Matrix4x4 final {
 
 };
 
+inline constexpr Vector3 operator*(const Vector3 &vector, const Matrix4x4 &matrix) {
+	Vector3 result;
+	result.x = matrix.m[0][0] * vector.x + matrix.m[1][0] * vector.y + matrix.m[2][0] * vector.z + matrix.m[3][0];
+	result.y = matrix.m[0][1] * vector.x + matrix.m[1][1] * vector.y + matrix.m[2][1] * vector.z + matrix.m[3][1];
+	result.z = matrix.m[0][2] * vector.x + matrix.m[1][2] * vector.y + matrix.m[2][2] * vector.z + matrix.m[3][2];
+	float w = matrix.m[0][3] * vector.x + matrix.m[1][3] * vector.y + matrix.m[2][3] * vector.z + matrix.m[3][3];
+	assert(w != 0.0f);
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+	return result;
+}
+
 inline constexpr Matrix4x4 MakeIdentity4x4() {
 	Matrix4x4 result;
 	for (size_t i = 0; i < 4; i++) {
@@ -85,15 +98,36 @@ inline Matrix4x4 MakeAffineMatrix(const Vector3 &scale, const Vector3 &rotate, c
 	return MakeScaleMatrix(scale) * MakeRotateMatrix(rotate) * MakeTranslateMatrix(translate);
 }
 
-inline constexpr Vector3 operator*(const Vector3 &vector, const Matrix4x4 &matrix) {
-	Vector3 result;
-	result.x = matrix.m[0][0] * vector.x + matrix.m[1][0] * vector.y + matrix.m[2][0] * vector.z + matrix.m[3][0];
-	result.y = matrix.m[0][1] * vector.x + matrix.m[1][1] * vector.y + matrix.m[2][1] * vector.z + matrix.m[3][1];
-	result.z = matrix.m[0][2] * vector.x + matrix.m[1][2] * vector.y + matrix.m[2][2] * vector.z + matrix.m[3][2];
-	float w = matrix.m[0][3] * vector.x + matrix.m[1][3] * vector.y + matrix.m[2][3] * vector.z + matrix.m[3][3];
-	assert(w != 0.0f);
-	result.x /= w;
-	result.y /= w;
-	result.z /= w;
+inline Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
+	Matrix4x4 result;
+	float f = 1.0f / std::tan(fovY / 2.0f);
+	float d = farClip - nearClip;
+	result.m[0][0] = f / aspectRatio;
+	result.m[1][1] = f;
+	result.m[2][2] = farClip / d;
+	result.m[2][3] = 1.0f;
+	result.m[3][2] = -nearClip * farClip / d;
+	return result;
+}
+
+inline constexpr Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
+	Matrix4x4 result = MakeIdentity4x4();
+	result.m[0][0] = 2.0f / (right - left);
+	result.m[1][1] = 2.0f / (top - bottom);
+	result.m[2][2] = 1.0f / (farClip - nearClip);
+	result.m[3][0] = (left + right) / (left - right);
+	result.m[3][1] = (top + bottom) / (bottom - top);
+	result.m[3][2] = nearClip / (nearClip - farClip);
+	return result;
+}
+
+inline constexpr Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth) {
+	Matrix4x4 result = MakeIdentity4x4();
+	result.m[0][0] = width / 2.0f;
+	result.m[1][1] = -height / 2.0f;
+	result.m[2][2] = maxDepth - minDepth;
+	result.m[3][0] = left + width / 2.0f;
+	result.m[3][1] = top + height / 2.0f;
+	result.m[3][2] = minDepth;
 	return result;
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <Novice.h>
 #include "Matrix4x4.h"
 
-const char kWindowTitle[] = "LE2A_03_クラタ_ユウキ_MT3_00_05";
+const char kWindowTitle[] = "LE2A_03_クラタ_ユウキ_MT3_01_00";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -13,9 +13,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
-	Vector3 scale{ 1.2f, 0.79f, -2.1f };		// 拡大率
-	Vector3 rotate{ 0.4f, 1.43f, -0.8f };		// 回転角度
-	Vector3 translate{ 2.7f, -4.15f, 1.57f };	// 移動量
+	const int kRowHeight = 20;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -30,7 +28,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
+		Matrix4x4 orthographicMatrix = MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
+		Matrix4x4 perspectiveFovMatrix = MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
 
 		///
 		/// ↑更新処理ここまで
@@ -40,7 +40,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		worldMatrix.ScreenPrintf(0, 0, "worldMatrix");
+		orthographicMatrix.ScreenPrintf(0, 0, "orthographicMatrix");
+		perspectiveFovMatrix.ScreenPrintf(0, kRowHeight * 5, "perspectiveFovMatrix");
+		viewportMatrix.ScreenPrintf(0, kRowHeight * 10, "viewportMatrix");
 
 		///
 		/// ↑描画処理ここまで

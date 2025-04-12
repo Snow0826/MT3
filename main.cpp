27184d6dd91f40dc75d11699bc46938b31821018
Yuki Kurata <k024g0109@g.neec.ac.jp>
2025-04-12 18:32:32 +0900

    ポリゴンの描画(修正)

--- main.cpp
+++ main.cpp
@@ -15,14 +15,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
+	// 初期化
 	Vector3 v1{ 1.2f, -3.9f, 2.5f };
 	Vector3 v2{ 2.8f, 0.4f, -1.3f };
 	Vector3 rotate{};
 	Vector3 translate{};
 	const Vector3 kLocalVertices[3] = {
-		{ 0.0f, 1.0f, 0.0f },
-		{ -1.0f, -1.0f, 0.0f },
-		{ 1.0f, -1.0f, 0.0f }
+		{ 0.0f, 0.5f, 0.0f },
+		{ -0.5f, -0.5f, 0.0f },
+		{ 0.5f, -0.5f, 0.0f }
 	};
 
 	// ウィンドウの×ボタンが押されるまでループ
@@ -38,6 +39,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		// 回転と移動
 		rotate.y += 0.03f;
 
 		if (keys[DIK_W]) {
@@ -56,8 +58,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			translate.x += 0.1f;
 		}
 
+		// 外積
 		Vector3 cross = v1.cross(v2);
 
+		// 描画座標の計算
 		Matrix4x4 worldMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, rotate, translate);
 
 		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, -5.0f });
@@ -82,8 +86,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
+		// 外積の結果を表示
 		cross.ScreenPrintf(0, 0, "Cross");
 
+		// 三角形を描画
 		Novice::DrawTriangle(
 			static_cast<int>(screenVertices[0].x),
 			static_cast<int>(screenVertices[0].y),

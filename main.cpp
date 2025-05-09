4880a9db0114e89f4a465e313d76545179b5fa50
Kurata Yuki <k024g0109@g.neec.ac.jp>
2025-05-09 17:06:44 +0900

    ベクトルの射影と最近接点の実装

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <Novice.h>
 #include "Matrix4x4.h"
 #include "Draw.h"
+#include "Collision.h"
 #include <imgui.h>
 
-constexpr char kWindowTitle[] = "LE2A_03_クラタ_ユウキ_MT3_01_00";
-constexpr int kWindowWidth = 1280;	// ウィンドウの幅
-constexpr int kWindowHeight = 720;	// ウィンドウの高さ
+constexpr char kWindowTitle[] = "LE2A_03_クラタ_ユウキ_MT3_02_00";
+constexpr int32_t kWindowWidth = 1280;	// ウィンドウの幅
+constexpr int32_t kWindowHeight = 720;	// ウィンドウの高さ
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -21,10 +22,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 cameraTranslate = { 0.0f, 1.9f, -6.49f };
 	Vector3 cameraRotate = { 0.26f, 0.0f, 0.0f };
 
-	// 球の初期化
-	Sphere sphere;
-	sphere.center = { 0.0f, 0.0f, 3.4f };
-	sphere.radius = 1.0f;
+	// 点と線分の初期化
+	Segment segment{ {-2.0f, -1.0f, 0.0f}, {3.0f, 2.0f, 2.0f} };
+	Vector3 point{ -1.5f, 0.6f, 0.6f };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -38,6 +38,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
+
+		// 正射影ベクトルと最近接点
+		Vector3 project = Project(point - segment.origin, segment.diff);
+		Vector3 closestPoint = ClosestPoint(point, segment);
+
+		// 描画用の点の初期化
+		Sphere pointSphere{ point, 0.01f };
+		Sphere closestPointSphere{ closestPoint, 0.01f };
 
 		// レンダリングパイプライン
 		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, cameraRotate, cameraTranslate);
@@ -57,15 +65,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		// グリッドを描画
 		DrawGrid(viewProjectionMatrix, viewportMatrix);
 
-		// 球を描画
-		DrawSphere(sphere, viewProjectionMatrix, viewportMatrix, BLACK);
+		// 線分を描画
+		Vector3 start = segment.origin * viewProjectionMatrix * viewportMatrix;
+		Vector3 end = (segment.origin + segment.diff) * viewProjectionMatrix * viewportMatrix;
+		Novice::DrawLine(
+			static_cast<int32_t>(start.x),
+			static_cast<int32_t>(start.y),
+			static_cast<int32_t>(end.x),
+			static_cast<int32_t>(end.y), WHITE);
+
+		// 点を描画
+		DrawSphere(pointSphere, viewProjectionMatrix, viewportMatrix, RED);
+		DrawSphere(closestPointSphere, viewProjectionMatrix, viewportMatrix, BLACK);
 
 		// ImGuiを描画
 		ImGui::Begin("Debug");
-		ImGui::DragFloat3("CameraTranslate", &cameraTranslate.x, 0.01f);
-		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
-		ImGui::DragFloat3("SphereCenter", &sphere.center.x, 0.01f);
-		ImGui::DragFloat("SphereRadius", &sphere.radius, 0.01f);
+		ImGui::InputFloat3("Point", &point.x);
+		ImGui::InputFloat3("Segment origin", &segment.origin.x);
+		ImGui::InputFloat3("Segment diff", &segment.diff.x);
+		ImGui::InputFloat3("Project", &project.x, "%.3f", ImGuiInputTextFlags_ReadOnly);
 		ImGui::End();
 
 		///
